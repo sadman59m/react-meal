e725d96400ac6a053439d64fc128f74261247cdf
Sadman Sakib <mahmudshadman@gmail.com>
2023-08-03 22:23:17 +0600

    firebase backend, loading UI

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,42 @@
+import React, { useState, useEffect } from "react";
+
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import Loading from "../UI/Loading";
 import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const getMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        "https://react-http-b9988-default-rtdb.firebaseio.com/meals"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
+      const fetchedMeals = [];
+      for (let key in data) {
+        fetchedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(fetchedMeals);
+      setIsLoading(false);
+    };
+    getMeals().catch((err) => {
+      setError(err.message);
+    });
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -40,10 +46,28 @@ const AvailableMeals = () => {
     />
   ));
 
+  let content;
+
+  if (isLoading) {
+    content = (
+      <section className={classes.content}>
+        <Loading />
+      </section>
+    );
+  }
+  // if (error) {
+  //   content = (
+  //     <p className={`${classes.content} ${classes.error}`}>
+  //       Somethig went wrong! Try again later.
+  //     </p>
+  //   );
+  // }
+
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {!isLoading && !error && <ul>{mealsList}</ul>}
+        {content}
       </Card>
     </section>
   );
---
+++ src/components/UI/Loading.js
@@ -0,0 +1,14 @@
+import classes from "./Loading.module.css";
+
+const Loading = () => {
+  return (
+    <div className={classes.loading}>
+      <p>Loading</p>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
